900fe4d05eb49918902650adffcd085e93e7e373
Michał Ogiński <michaloginski15@gmail.com>
2024-03-11 20:42:39 +0100

    stack lol

---
+++ main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct StackElement {
+    int Data;
+    struct StackElement* Last;
+} StackElementType;
+
+// Podwójny wskaźnik po to żeby StackElementType** wskazywał na wskaźnik (a nie na dane) przez co mógł go edytować
+void Push(StackElementType** Top, int Number);
+int Pop(StackElementType** Top);
+void Peek(StackElementType** Top);
+
+int main() {
+
+    StackElementType* Top;
+    Top = NULL;
+
+    int Option = -1;
+    int Number = -1;
+
+    while(Option != 0)
+    {
+        printf("1. Push element to the stack \n");
+        printf("2. Pop element from the stack \n");
+        printf("3. Print element from the stack \n");
+        printf("0. Exit \n");
+
+        scanf("%i", &Option);
+
+        switch(Option)
+        {
+            case 0:
+                return 0;
+                break;
+            case 1:
+                printf("Pleas provide a integer: \n");
+                scanf("%i", &Number);
+                Push(&Top, Number);
+                break;
+            case 2:
+                Pop(&Top);
+                break;
+            case 3:
+                Peek(&Top);
+                break;
+            default:
+                printf("Choose a proper option \n");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void Push(StackElementType** Top, int Number)
+{
+    if(*Top == NULL)
+    {
+        *Top = (StackElementType*) malloc(sizeof(StackElementType));
+        (*Top)->Data = Number;
+        (*Top)->Last = NULL;
+    }
+    else
+    {
+        StackElementType* NewElement;
+        NewElement = (StackElementType*) malloc(sizeof(StackElementType));
+        NewElement->Data = Number;
+        NewElement->Last = *Top;
+        *Top = NewElement;
+    }
+}
+
+int Pop(StackElementType** Top)
+{
+    int DataToPop = 0;
+
+    if(*Top != NULL)
+    {
+        DataToPop = (*Top)->Data;
+
+        StackElementType* Temp = NULL;
+        Temp = (*Top)->Last;
+        free(*Top);
+        *Top = Temp;
+    }
+
+    return DataToPop;
+}
+
+void Peek(StackElementType** Top)
+{
+    if(*Top != NULL)
+    {
+        printf("Element from the top of the stack: %i \n", (*Top)->Data);
+    }
+}
